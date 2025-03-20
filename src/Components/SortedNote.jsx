a3d098eed4672434487a291aa6cfc3597ed9b045
Sagor Majomder <sagormajomder@gmail.com>
2025-03-20 16:25:31 +0600

    add and organize components

---
+++ src/Components/SortedNote.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import Button from "./common/Button";
+
+SortedNote.propTypes = {
+  setSortBy: PropTypes.func.isRequired,
+};
+
+function SortedNote({ setSortBy }) {
+  return (
+    <div className="sm:text-body text-caption flex grow-1 flex-wrap gap-2">
+      <Button
+        type="button"
+        style="rounded-sm px-3 py-1"
+        onClick={() => setSortBy("oldest")}
+      >
+        Sort by oldest
+      </Button>
+      <Button
+        type="button"
+        style=" rounded-sm px-3 py-1"
+        onClick={() => setSortBy("latest")}
+      >
+        Sort by latest
+      </Button>
+    </div>
+  );
+}
+
+export default SortedNote;
